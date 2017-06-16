94c65166e40d373e552c1a88480ce7eaa41227c4
Kanstantsin Astapuk <Kanstantsin_Astapuk@epam.com>
2017-06-16 15:57:46 +0300

    end2 commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+function success(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    var altitude = position.coords.altitude;
+    var speed = position.coords.speed;
 
+    document.write("Широта: " + latitude + "<br/>");
+    document.write("Долгота: " + longitude + "<br/>");
+    document.write("Высота: " + altitude + "<br/>");
+    document.write("Скорость перемещения: " + speed + "<br/>");
+};
+
+function error(obj) {
+    document.write("Ошибка при определении положения");
+};
+navigator.geolocation.getCurrentPosition(success, error);
